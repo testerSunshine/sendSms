3a40f2e75c5ebc5e3f43904531f35396b46159b7
wen <931128603@qq.com>
2017-06-19 19:25:38 +0800

    pull code

--- getWeather.py
+++ getWeather.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 import json
+import random
 from time import sleep
 import myUrllib
 
@@ -27,8 +28,29 @@ class Weather(object):
                 print(result["status"])
             sleep(1)
 
+    def sendTemplate(self):
+        template = [
+            "新的一天又来啦，给小主念早安早安早安，重要的事情说三遍，",
+            "早安哦，催小主快带你起床啦，",
+            "坚持不懈的小主奉上早安，我是不会忘记的，",
+            "早晨，是一个美妙的开端，小主早安哦，",
+            "小主，我有一千种给你说早安的方式，今天第一千种,",
+            "每天都是改变命运的机会，小主，早安呐，",
+            "早安，太阳，早安，地球，早安，中国，早安，亲爱的小主，快起来了，",
+            "我想告诉全世界的人，小主，早安！",
+            "太阳冉冉升起，清风柔柔吹起,早安，",
+            "让爱你的人放心，让恨你的人失落, 一碗鸡汤请小主喝，早安，",
+            "我想你一定很忙，所以只看前三个字就好啦，早安呀！",
+            "想不出来今天说啥了，嗯，早安，",
+            "掀被而起，君临天下,haha,",
+            "帅的人已醒 丑的人还在沉睡,丑的是指的我，早安，",
+            "good   morning。。。。。",
+            "晚安，哦，说错了，是早安，小主，"
+        ]
+        self.message.append(template[random.randint(0, len(template))])
+
     def sendMessageByTemperature(self):
-        self.message.append("新的一天又来啦，给小主念早安早安早安，重要的事情说三遍，今天是{}\n".format(self.weatherInfo["date"]))
+        self.message.append("今天是{}\n".format(self.weatherInfo["date"]))
         self.message.append("%s最高温度 %s，最低温度 %s\n" % (self.city_name, self.weatherInfo["high"].split(" ")[1], self.weatherInfo["low"].split(" ")[1]))
 
     def sendMessageByType(self):
@@ -37,11 +59,11 @@ class Weather(object):
         elif self.weatherInfo["type"] == "阴":
             self.message.append("天气是:{}，天气一般，小主呆在家里好好追剧最好".format(self.weatherInfo["type"]))
         elif self.weatherInfo["type"] == "雾":
-            self.message.append("天气是:{}，出门眼睛要擦亮哦，小主要看好老公！".format(self.weatherInfo["type"]))
+            self.message.append("天气是:{}，出门眼睛要擦亮哦，提醒小主要看好老公！".format(self.weatherInfo["type"]))
         elif self.weatherInfo["type"] == "雨夹雪":
             self.message.append("天气是:{}，把伞带好，小主还是别出门啦，如果硬要出，把家里的皮大衣拿来".format(self.weatherInfo["type"]))
         elif self.weatherInfo["type"] == "雷阵雨" or self.weatherInfo["type"] == "小雨" or self.weatherInfo["type"] == "大雨" or self.weatherInfo["type"] == "中雨" or self.weatherInfo["type"] == "雨":
-            self.message.append("天气是:{}，把伞伞伞带好，重要的事情说三遍，下班自当来接小主回家哦！".format(self.weatherInfo["type"]))
+            self.message.append("天气是:{}，把伞伞伞带好，重要的事情说三遍，雨天路滑，回家当心，路上别看手机".format(self.weatherInfo["type"]))
         elif self.weatherInfo["type"] == "暴雨":
             self.message.append("天气是:{}，今天要不出去了，太可怕了！".format(self.weatherInfo["type"]))
         elif self.weatherInfo["type"] == "大雪" or self.weatherInfo["type"] == "小雪" or self.weatherInfo["type"] == "中雪":
@@ -54,6 +76,7 @@ class Weather(object):
 
     def main(self):
         self.getWeatherInfo()
+        self.sendTemplate()
         self.sendMessageByTemperature()
         self.sendMessageByType()
         return "".join(self.message)
--- sendMessage.py
+++ sendMessage.py
@@ -1,12 +1,91 @@
 # -*- coding: utf8 -*-
+import os
+import urllib2
+
+import yaml
 import myUrllib
+from getWeather import Weather
 
-class SendMessage(object):
-    def __init__(self, phone, params):
-        self.phone = phone
-        self.params = params
-        self.APPKEY = "50792ac04a966ce8be51ccbbf55964c2"
-        self.AppSecret = "77363f51c135"
 
-    def send(self):
-        sendUrl = "'https://api.netease.im/sms/sendtemplate.action"
+class sendMessage(object):
+    def __init__(self):
+        self.smsAgrs = self._get_yaml()["sms"]
+        self.weather = self._get_yaml()["Weather"]
+        self.phone = self.smsAgrs["phone"]
+        self.name = self.smsAgrs["name"]
+        self.pwd = self.smsAgrs["pwd"]
+        self.sign = self.smsAgrs["sign"]
+
+    def _getBalance(self):
+        """
+        获取余额
+        :return:1 代表 余额不足  0 代表余额充足
+        """
+        sendUrl = "http://web.cr6868.com/asmx/smsservice.aspx?name={}&pwd={}&type=balance".format(self.name, self.pwd)
+        resultB = myUrllib.get(sendUrl)
+        if resultB is not None:
+            spResultB = resultB.split(",")
+            code = spResultB[0]
+            banlance = int(spResultB[1])
+            if code == "0" and banlance > 0:
+                print("当前短信剩余条数：{}".format(str(banlance)))
+                return 0
+        else:
+            return 1
+
+    def _sendInfo(self, content):
+        """
+        发送短信内容
+        :return: 0 代表发送成功 1 代表发送失败
+        """
+        content = content.encode("utf-8")
+        content = urllib2.quote(content)
+        sendUrl = "http://web.cr6868.com/asmx/smsservice.aspx?name={}&pwd={}&content={}&mobile={}&stime=&sign={}&type=pt&extno=".format(self.name, self.pwd, content, self.phone, self.sign)
+        # sendUrl = "http://web.cr6868.com/asmx/smsservice.aspx"
+        # data = {
+        #     "name": self.name,
+        #     "pwd": self.pwd,
+        #     "content": content,
+        #     "mobile": self.phone,
+        #     "sign": self.sign,
+        # }
+        infoResult = myUrllib.get(sendUrl)
+        if infoResult is not None:
+            code = infoResult.split(",")[0]
+            if int(code) == 0:
+                return 0
+            elif int(code) == 1:
+                print("当前短信发送失败，含有铭感词汇，短信内容为：{}".format(content))
+                return 1
+        else:
+            print("接口返回值为空")
+
+    def _get_yaml(self):
+        """
+        解析yaml
+        :return: s  字典
+        """
+        path = os.path.join(os.path.dirname(__file__) + '/conf.yaml')
+        f = open(path)
+        s = yaml.load(f)
+        f.close()
+        if s is not None:
+            return s
+        else:
+            print("conf content is None")
+
+    def main(self):
+        balance = self._getBalance()
+        if int(balance) == 0:
+            content = Weather(self.weather["city_name"]).main()
+            status = self._sendInfo(content)
+            if status == 0:
+                print("当前短信发送成功，发送内容为：{}".format(content))
+            else:
+                print("当前短信发送失败，可能有敏感字符，发送内容为：{}".format(content))
+        elif int(balance) == 1:
+            print("余额不足")
+
+if __name__ == '__main__':
+    s = sendMessage()
+    s.main()
